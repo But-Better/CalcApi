e5e794b072b84730f08ec1606ea78a9cfba16698
note <kitsu-geissler@t-online.de>
2021-10-24 01:18:28 +0200

    initial commit

---
+++ src/main/java/de/butbetter/keksys/CalculatorApiApplication.java
@@ -0,0 +1,13 @@
+package de.butbetter.keksys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/butbetter/keksys/CalculatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.butbetter.keksys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
