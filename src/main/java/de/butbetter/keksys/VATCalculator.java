67c6127b2bb66b3db5be4cb8e5bb712bc962cc9c
note <kitsu-geissler@t-online.de>
2021-11-03 19:06:56 +0100

    throw exception, when calculated value is infinite

--- src/main/java/de/butbetter/keksys/VATCalculator.java
+++ src/main/java/de/butbetter/keksys/VATCalculator.java
@@ -10,10 +10,15 @@ public class VATCalculator implements Calculator {
 	private static final Logger log = LoggerFactory.getLogger(VATCalculator.class);
 
 	@Override
-	public Float calculateVAT(float input, float percent) {
+	public Float calculateVAT(float input, float percent) throws IllegalArgumentException {
 		input = Math.max(0, input);
 		percent = Math.max(0, percent);
-
-		return ((input / 100) * percent) + input;
+		float ret = ((input / 100) * percent) + input;
+		if (Float.isInfinite(ret)) {
+			String message = "the calculated value is larger than the given type, therefore it will be infinite";
+			log.error(message);
+			throw new IllegalArgumentException(message);
+		}
+		return ret;
 	}
 }
