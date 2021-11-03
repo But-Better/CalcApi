3218c4fd2b8b951c56cdcae6977b9f1f109965b4
note <kitsu-geissler@t-online.de>
2021-11-03 18:35:33 +0100

    update doc of calculateVat

--- src/main/java/de/butbetter/keksys/Calculator.java
+++ src/main/java/de/butbetter/keksys/Calculator.java
@@ -1,5 +1,18 @@
 package de.butbetter.keksys;
 
 public interface Calculator {
+
+	/**
+	 * takes a value and a percentage,
+	 * and returns the value with the
+	 * given percentage of that value on op
+	 *
+	 * when given negative values, the value will
+	 * be instead changed to 0 and the rest will continue
+	 *
+	 * @param input input value
+	 * @param percent percentage to add
+	 * @return value plus percentage of that value
+	 */
 	Float calculateVAT(float input, float percent);
 }
