839761ea10c81e2cbc11dbd3a2674d6168a8ee65
kev <kevHTW@gmx.de>
2021-11-03 17:19:10 +0100

    Added first Unittest to try out autowired

---
+++ src/test/java/de/butbetter/keksys/CalculationControllerTests.java
@@ -0,0 +1,20 @@
+package de.butbetter.keksys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class CalculationControllerTests {
+
+    @Autowired
+    private CalcuationController calcuationController;
+
+    @Test
+    void contextLoads() {
+        assertThat(calcuationController).isNotNull();
+    }
+
+}
