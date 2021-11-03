7bcfc352fb6ab5e9b079af99d57bbbcdd9bcbc46
kev <kevHTW@gmx.de>
2021-11-03 17:13:20 +0100

    Add dummy controller

--- src/main/java/de/butbetter/keksys/CalcuationController.java
+++ src/main/java/de/butbetter/keksys/CalcuationController.java
@@ -1,2 +1,14 @@
-package de.butbetter.keksys;public class CalcuationController {
+package de.butbetter.keksys;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalcuationController {
+
+    @GetMapping("/")
+    public Object calculation() {
+        return new Object();
+    }
 }
