5a4e8f78842179e38b22dc3d10f88991b0b92bf6
note <kitsu-geissler@t-online.de>
2021-11-04 12:00:15 +0100

    add throw of IllegalArgumentException if calculated price is infinite (https://github.com/But-Better/CalcApi/pull/6#discussion_r742296781)

--- src/main/java/de/butbetter/keksys/VATCalculator.java
+++ src/main/java/de/butbetter/keksys/VATCalculator.java
@@ -10,11 +10,15 @@ public class VATCalculator implements Calculator {
 	private static final Logger log = LoggerFactory.getLogger(VATCalculator.class);
 
 	@Override
-	public Float calculateVAT(float input, float percent) throws IllegalArgumentException {
-		input = Math.max(0, input);
-		percent = Math.max(0, percent);
-		float return_value = ((input / 100) * percent) + input;
-		if (Float.isInfinite(return_value)) {
+	public Float calculateVAT(float price, float percent) throws ArithmeticException, IllegalArgumentException {
+		if (price < 0 || percent < 0) {
+			String message = "either the given input: " + price + " or the percentage: " + percent + " was negative, " +
+					"which is not supported by this method";
+			log.error(message);
+			throw new IllegalArgumentException(message);
+		}
+		float priceWithVAT = ((price / 100) * percent) + price;
+		if (Float.isInfinite(priceWithVAT)) {
 			String message = "the calculated value is larger than the given type, therefore it will be infinite";
 			log.error(message);
 			throw new ArithmeticException(message);
