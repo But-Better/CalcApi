97ff476e760bf4dd3676741a644e99ccdb3d6242
note <kitsu-geissler@t-online.de>
2021-11-04 12:05:50 +0100

    update parameter name of calculateVAT method for better clarity

--- src/main/java/de/butbetter/keksys/Calculator.java
+++ src/main/java/de/butbetter/keksys/Calculator.java
@@ -3,17 +3,14 @@ package de.butbetter.keksys;
 public interface Calculator {
 
 	/**
-	 * takes a value and a percentage,
+	 * takes a price and a percentage,
 	 * and returns the value with the
 	 * given percentage of that value on top
 	 *
-	 * when given negative values, the value will
-	 * be instead changed to 0 and the rest will continue
-	 *
-	 * @param input input value
+	 * @param price input value
 	 * @param percent percentage to add
 	 * @return value plus percentage of that value
 	 * @throws IllegalArgumentException thrown, if calculated value is bigger than float allows
 	 */
-	Float calculateVAT(float input, float percent) throws IllegalArgumentException;
+	Float calculateVAT(float price, float percent) throws IllegalArgumentException;
 }
