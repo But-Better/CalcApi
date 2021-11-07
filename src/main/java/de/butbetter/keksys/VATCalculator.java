5918556d14ed7633f1099b8fd357ad2b33cae57d
note <kitsu-geissler@t-online.de>
2021-11-07 11:48:23 +0100

    remove unneeded checks, they'll be added into another class

--- src/main/java/de/butbetter/keksys/VATCalculator.java
+++ src/main/java/de/butbetter/keksys/VATCalculator.java
@@ -10,31 +10,7 @@ public class VATCalculator implements Calculator {
 	private static final Logger log = LoggerFactory.getLogger(VATCalculator.class);
 
 	@Override
-	public Float calculateVAT(float price, float percent) throws ArithmeticException, IllegalArgumentException {
-		if (price < 0 || percent < 0) {
-			String message = "either the given input: " + price + " or the percentage: " + percent + " was negative, " +
-					"which is not supported by this method";
-			log.error(message);
-			throw new IllegalArgumentException(message);
-		}
-
-		float priceWithVAT = ((price / 100) * percent) + price;
-		return getInfinitySaveFloat(priceWithVAT);
-	}
-
-	/**
-	 * Float checker for infinity,
-	 *
-	 * @param input potential infinity float value
-	 * @return float
-	 * @throws ArithmeticException thrown, if input value is infinite
-	 */
-	private Float getInfinitySaveFloat(Float input) throws ArithmeticException {
-		if (Float.isInfinite(input)) {
-			String message = "the calculated value is larger than the given type, therefore it will be infinite";
-			log.error(message);
-			throw new ArithmeticException(message);
-		}
-		return input;
+	public Float calculateVAT(float price, float percent) {
+		return ((price / 100) * percent) + price;
 	}
 }
