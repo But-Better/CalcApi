0180fd1852d61cb260525451daaa0f40ab858f97
note <kitsu-geissler@t-online.de>
2021-11-07 23:15:34 +0100

    update decimal rounding, so it's not subject to some OS bullshittery

--- src/main/java/de/butbetter/keksys/VATCalculator.java
+++ src/main/java/de/butbetter/keksys/VATCalculator.java
@@ -4,15 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.text.DecimalFormat;
 
 @Service
 class VATCalculator implements Calculator {
 
 	private static final Logger log = LoggerFactory.getLogger(VATCalculator.class);
-
-	private static final DecimalFormat df_dot = new DecimalFormat("0.00");
-	private static final DecimalFormat df_comma = new DecimalFormat("0,00");
 
 	@Override
 	public Float calculateVAT(float price, float percent) {
@@ -25,10 +24,6 @@ class VATCalculator implements Calculator {
 	 * rounds the value up to a given decimal point
 	 */
 	private float roundTo2DecimalPoints(float value) {
-		try {
-			return Float.parseFloat(df_dot.format(value));
-		} catch (NumberFormatException e) {
-			return Float.parseFloat(df_comma.format(value));
-		}
+		return new BigDecimal(value).setScale(2, RoundingMode.HALF_UP).floatValue();
 	}
 }
