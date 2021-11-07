48e67e58709d493429243ccaa401958d15e44bae
note <kitsu-geissler@t-online.de>
2021-11-07 12:41:21 +0100

    add requestmapping with version and service name

--- src/main/java/de/butbetter/keksys/CalculationController.java
+++ src/main/java/de/butbetter/keksys/CalculationController.java
@@ -5,12 +5,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 
+
 @RestController
+@RequestMapping("calc/v1/")
 public class CalculationController {
 
     private static final Logger logger = LoggerFactory.getLogger(CalculationController.class);
