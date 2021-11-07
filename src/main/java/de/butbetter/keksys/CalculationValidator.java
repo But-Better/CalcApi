57340ec7abb19380ba2d2856afeb3e226dde39db
MathisHTW <Mathis.Schott@Student.HTW-Berlin.de>
2021-11-07 12:12:04 +0100

    refactor CalculationValdator

--- src/main/java/de/butbetter/keksys/CalculationValidator.java
+++ src/main/java/de/butbetter/keksys/CalculationValidator.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CalculationValidator implements Calculator{
+public class CalculationValidator implements Calculator {
 
     private static final Logger log = LoggerFactory.getLogger(CalculationValidator.class);
 
@@ -15,8 +15,8 @@ public class CalculationValidator implements Calculator{
 
     @Override
     public Float calculateVAT(float price, float percent) throws IllegalArgumentException, ArithmeticException {
-        below0(price,percent);
-        Float priceWithVAT = calculator.calculateVAT(price,percent);
+        below0(price, percent);
+        Float priceWithVAT = calculator.calculateVAT(price, percent);
         priceWithVATInfinityCheck(priceWithVAT);
         return priceWithVAT;
     }
@@ -24,11 +24,11 @@ public class CalculationValidator implements Calculator{
     /**
      * takes price and percent input for VAT Calculation and checks if they or one of them are/is below 0
      *
-     * @param price potentially below 0
+     * @param price   potentially below 0
      * @param percent potentially below 0
      * @throws IllegalArgumentException if both or one of the inputs is below 0
      */
-    private void below0(Float price, Float percent){
+    private void below0(Float price, Float percent) {
         if (price < 0 || percent < 0) {
             String message = "either the given input: " + price + " or the percentage: " + percent + " was negative, " +
                     "which is not supported by this method";
---
+++ src/test/java/de/butbetter/keksys/CalculationValidatorTest.java
@@ -0,0 +1,28 @@
+package de.butbetter.keksys;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculationValidatorTest {
+
+    Calculator calc;
+
+    @BeforeEach
+    void setup() {
+        calc = new CalculationValidator();
+    }
+
+    @Test
+    void calculateNegativeNumber() {
+        assertThrows(IllegalArgumentException.class, () -> calc.calculateVAT(-50, 19));
+        assertThrows(IllegalArgumentException.class, () -> calc.calculateVAT(100, -256));
+    }
+
+    @Test
+    void calculateMax() {
+        assertThrows(ArithmeticException.class, () -> calc.calculateVAT(Float.MAX_VALUE, 1));
+    }
+
+}
