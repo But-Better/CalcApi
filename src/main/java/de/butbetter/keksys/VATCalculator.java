f46b31a8bd15ea32046ea9428b299a927df91ceb
note <kitsu-geissler@t-online.de>
2021-11-07 23:01:00 +0100

    update float conversion

--- src/main/java/de/butbetter/keksys/VATCalculator.java
+++ src/main/java/de/butbetter/keksys/VATCalculator.java
@@ -11,7 +11,8 @@ class VATCalculator implements Calculator {
 
 	private static final Logger log = LoggerFactory.getLogger(VATCalculator.class);
 
-	private static final DecimalFormat df = new DecimalFormat("0.00");
+	private static final DecimalFormat df_dot = new DecimalFormat("0.00");
+	private static final DecimalFormat df_comma = new DecimalFormat("0,00");
 
 	@Override
 	public Float calculateVAT(float price, float percent) {
@@ -24,6 +25,10 @@ class VATCalculator implements Calculator {
 	 * rounds the value up to a given decimal point
 	 */
 	private float roundTo2DecimalPoints(float value) {
-		return Float.parseFloat(df.format(value));
+		try {
+			return Float.parseFloat(df_dot.format(value));
+		} catch (NumberFormatException e) {
+			return Float.parseFloat(df_comma.format(value));
+		}
 	}
 }
