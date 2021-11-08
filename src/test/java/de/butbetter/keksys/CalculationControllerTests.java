9ec77a6e54b0cc8907bc3c99d582f7df59c5c311
Kevin <Kevin-te@t-online.de>
2021-11-08 08:35:54 +0100

    Added new Tests for controller

--- src/test/java/de/butbetter/keksys/CalculationControllerTests.java
+++ src/test/java/de/butbetter/keksys/CalculationControllerTests.java
@@ -29,4 +29,34 @@ public class CalculationControllerTests {
         assertEquals(this.calcuationController.calculation(-1, 100).getStatusCode(), HttpStatus.BAD_REQUEST);
     }
 
+    @Test
+    void sendParameter0and0(){
+        assertEquals(this.calcuationController.calculation(0, 0).getStatusCode(), HttpStatus.OK);
+    }
+
+    @Test
+    void sendParameterMinus1andMinus1(){
+        assertEquals(this.calcuationController.calculation(-1, -1).getStatusCode(), HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    void sendParameter0andMinus1(){
+        assertEquals(this.calcuationController.calculation(0, -1).getStatusCode(), HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    void sendParameterFloatMaxAndMinus1(){
+        assertEquals(this.calcuationController.calculation(Float.MAX_VALUE, -1).getStatusCode(), HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    void sendParameterFloatMinAndMinus1(){
+        assertEquals(this.calcuationController.calculation(Float.MIN_VALUE, -1).getStatusCode(), HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    void sendParameterFloatMinAndMinusFloatMax(){
+        assertEquals(this.calcuationController.calculation(Float.MIN_VALUE, Float.MAX_VALUE).getStatusCode(), HttpStatus.OK);
+    }
+
 }
